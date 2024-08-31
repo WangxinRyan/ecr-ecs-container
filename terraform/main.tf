25dbd05b8c42419dd717ffd7d34ca44c94a1923b
Wang Xin <xin_wang_xin@hotmail.com>
2024-08-31 13:11:24 +0800

    7th commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,30 +17,9 @@ resource "aws_ecrpublic_repository" "demo-repository" {
   # image_tag_mutability = "IMMUTABLE"
 }
 
-resource "aws_ecr_repository_policy" "demo-repo-policy" {
-  repository = aws_ecr_repository.demo-repository.name
-  policy     = <<EOF
-  {
-    "Version": "2008-10-17",
-    "Statement": [
-      {
-        "Sid": "adds full ecr access to the demo repository",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:BatchGetImage",
-          "ecr:CompleteLayerUpload",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:GetLifecyclePolicy",
-          "ecr:InitiateLayerUpload",
-          "ecr:PutImage",
-          "ecr:UploadLayerPart"
-        ]
-      }
-    ]
-  }
-  EOF
+resource "aws_ecrpublic_repository_policy" "demo-repo-policy" {
+    repository_name =  = aws_ecrpublic_repository.demo-repository.name
+
 }
 
 module "ecs" {
